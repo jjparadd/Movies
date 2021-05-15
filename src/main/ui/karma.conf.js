2d1082f44f20038c37cd96fd3df13a95281499ec
Hung Vo <vo_ngoc_hung@yahoo.com>
2021-05-15 14:47:10 -0400

    add dockerfiles, build objects, circleci ci

--- src/main/ui/karma.conf.js
+++ src/main/ui/karma.conf.js
@@ -25,12 +25,14 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/ui'),
+      dir: require('path').join(__dirname, '../../../coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
-      ]
+        { type: 'text-summary' },
+        { type: 'lcovonly'},
+      ],
+      fixWebpackSourcePaths: true,
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
